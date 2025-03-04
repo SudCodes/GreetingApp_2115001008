fe1eb2163f8fed5941ad54080ee73bbc9b86a2b8
Sudhanshu Agrawal <iamsudhanshu1001@gmail.com>
2025-03-04 22:49:43 +0530

    [Sudhanshu] Add: Added UC2 Code

--- RepositoryLayer/Service/GreetingRL.cs
+++ RepositoryLayer/Service/GreetingRL.cs
@@ -9,6 +9,15 @@ namespace RepositoryLayer.Service
 {
     public class GreetingRL : IGreetingRL
     {
+        private string GreetingMSG = "Hello World";
 
+        private string GetMessage()
+        {
+            return GreetingMSG;
+        }
+        public string GetGreetingRL()
+        {
+            return GetMessage();
+        }
     }
 }
